29224dbbddbe97a4907e2c2fe4d3817e8055e4e3
Rohan Patil <rohanpatil1216@gmail.com>
2026-03-03 19:38:38 +0530

    Initial commit: Project setup backend

---
+++ code-migration/src/test/java/com/codengrow/code/migration/CodeMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codengrow.code.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
